1fa5ad430a01e68c7261c5f4227f9c2bfa60bd5d
Yoann Faraus <yofardev@gmail.com>
2026-01-07 12:33:28 +0100

    refactor: create core module structure and move shared code

---
+++ lib/core/services/cache_service.dart
@@ -0,0 +1,27 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class CacheService {
+  static const String _folderPathKey = 'folderPath';
+  static Future<void> saveFolderPath(String path) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_folderPathKey, path);
+  }
+
+  static Future<String?> loadFolderPath() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_folderPathKey);
+  }
+
+  static Future<void> saveMacosBookmark({
+    required String bookmark,
+    required String folderPath,
+  }) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString(folderPath, bookmark);
+  }
+
+  static Future<String?> loadMacosBookmark({required String folderPath}) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString(folderPath);
+  }
+}
